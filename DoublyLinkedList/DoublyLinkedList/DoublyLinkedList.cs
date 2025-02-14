28fb04114c4073fc6c037b7b5dd9c8873837051b
BasarEkinci <basarekinci02@outlook.com>
2025-02-14 16:49:24 +0300

    DoublyLinkedList Completed

--- DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
+++ DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
@@ -41,7 +41,7 @@
         public void PrintList()
         {
             Node currentNode = head;
-            Console.Write("\nList: ");
+            Console.Write("List: ");
             while (currentNode != null)
             {
                 Console.Write(currentNode.Data + " ");
@@ -74,7 +74,7 @@
 
         public Node DeleteLast()
         {
-            if (head == null) return null;
+            if (length == 0) return null;
             Node nodeToDelete = tail;
 
             if(length == 1)
@@ -88,7 +88,151 @@
                 tail.Next = null;
                 nodeToDelete.Prev = null;
             }
+            length--;
             return nodeToDelete;
+        }
+
+        public Node DeleteFirst()
+        {
+            if (length == 0) return null;
+
+            Node nodeToDelete = head;
+            if (length == 1)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                head = head.Next;
+                head.Prev = null;
+                nodeToDelete.Next = null;
+            }
+            length--;
+            return nodeToDelete;
+        }
+
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            if(length == 0)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                newNode.Next = head;
+                head.Prev = newNode;
+                head = newNode;
+            }
+            length++;
+        }
+
+        public Node GetNodeAt(int index)
+        {
+            if (length == 0) return null;
+            if (index < 0 || index >= length) return null;
+
+            if (length == 1)
+            {
+                return head;
+            }
+            else
+            {
+                Node temp;
+                int tempIndex;
+                int n = length / 2;
+                if (n >= index)
+                {
+                    tempIndex = length - 1;
+                    temp = tail;
+
+                    while (tempIndex != index)
+                    {
+                        tempIndex--;
+                        temp = temp.Prev;
+                    }
+                }
+                else
+                {
+                    tempIndex = 0;
+                    temp = head;
+                    while (tempIndex != index)
+                    {
+                        tempIndex++;
+                        temp = temp.Next;
+                    }
+                }
+                return temp;
+            }
+        }
+
+        public void SetNode(int index, int newData)
+        {
+            Node currentNode = GetNodeAt(index);
+            if (currentNode == null) return;
+            currentNode.Data = newData;
+        }
+
+        public bool InsertNodeAt(int index, int newData)
+        {
+            if (index < 0 || index > length) return false;
+
+            if(index == length)
+            {
+                Append(newData);
+            }
+            else if(index == 0)
+            {
+                AddFirst(newData);
+            }
+            else
+            {
+                Node newNode = new Node(newData);
+                Node prevNode = GetNodeAt(index - 1);
+                Node nextNode = GetNodeAt(index);
+
+                newNode.Next = nextNode;
+                newNode.Prev = prevNode;
+
+                prevNode.Next = newNode;
+                nextNode.Prev = newNode;
+                length++;
+            }
+            return true;
+        }
+
+        public Node DeleteNodeAt(int index)
+        {
+            if(index >= 0 && index < length)
+            {
+                
+                if (index == 0)
+                {
+                    return DeleteFirst();
+                }
+                else if (index == length - 1)
+                {
+                    return DeleteLast();
+                }
+                else
+                {
+                    Node nodeToDelete = GetNodeAt(index);
+
+                    nodeToDelete.Next.Prev = nodeToDelete.Prev;
+                    nodeToDelete.Prev.Next = nodeToDelete.Next;
+
+                    nodeToDelete.Next = null;
+                    nodeToDelete.Prev = null;
+                    length--;
+                    return nodeToDelete;
+                }
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
--- DoublyLinkedList/DoublyLinkedList/Program.cs
+++ DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DoublyLinkedList
+﻿using System.Net.WebSockets;
+
+namespace DoublyLinkedList
 {
     internal class Program
     {
@@ -7,9 +9,15 @@
             DoublyLinkedList dlinkedList = new DoublyLinkedList();
 
             dlinkedList.Append(1);
+            dlinkedList.Append(2);
+            dlinkedList.Append(3);
+            dlinkedList.Append(4);
             dlinkedList.PrintList();
-            dlinkedList.DeleteLast();
+            Console.WriteLine("\nLenght: " + dlinkedList.GetLength() + "\n---------------------");
+            Node deletedNode = dlinkedList.DeleteNodeAt(2);
             dlinkedList.PrintList();
+            Console.Write("\nDeleted Node: " + deletedNode?.Data);
+            Console.WriteLine("\nLenght: " + dlinkedList.GetLength() + "\n---------------------");
         }
     }
 }
